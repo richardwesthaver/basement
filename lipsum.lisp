d1188de284ed5dd4d600c9f69a4fd7d55e474fcc
Richard Westhaver <ellis@rwest.io>
2026-06-06 22:22:30 -0400

    lipsum/dired-vc init

---
+++ lipsum.lisp
@@ -0,0 +1,19 @@
+;;; lipsum.lisp --- Lorem Ipsum 
+
+;; Lorem Ipsum(s) Generator for Lisp
+
+;;; Commentary:
+
+;; In the simplest case does as you would expect - generates pseudo-random
+;; sequences of prepared latin statements.
+
+;; In the future we may extend this to support custom generators based on
+;; user-supplied corpus (*lipsum-corpus*) as well as a light protocol for
+;; mixing into higher-level objects such as ORGAN:ORG-DOCUMENT.
+
+;;; Code:
+(in-package :rt/lipsum)
+
+;; sentences
+
+;; paragraphs
